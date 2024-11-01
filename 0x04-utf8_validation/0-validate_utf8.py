f1790a60d35540fb5e1183656dd05a73858f846a
DYung26 <theadstore1407@gmail.com>
2024-10-31 21:30:42 -0700

    testing...

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """
-This module provides a utility to validate whether a given list of integers
-represents a valid UTF-8 encoding. Each integer is treated as a byte, and the
-module confirms if these bytes form a valid UTF-8 encoded sequence.
+This method determines if a
+given data set represents a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
     """
-    Checks if a list of integers represents a valid UTF-8 encoding.
-
-    Parameters:
-    data (list of int): A list of integers where each integer should be in the
-    range 0-255, representing a byte.
-
-    Returns:
-    bool: True if the list represents a valid UTF-8 encoding, False otherwise.
-
-    The function converts the list of integers to bytes and attempts to decode
-    them as UTF-8. If decoding succeeds, the data is valid UTF-8; otherwise,
-    an exception is caught, and the function returns False.
+    returns True if data is a valid
+    UTF-8, otherwise it'll be False
     """
-    if not all(isinstance(x, int) and 0 <= x <= 0xFF for x in data):
-        return False
-    try:
-        bytes(data).decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
+    num_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
