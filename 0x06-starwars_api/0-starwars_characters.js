59a804e0e5ff0634fc33269b63b0da703f749370
DYung26 <theadstore1407@gmail.com>
2024-11-17 18:13:31 -0800

    `Star Wars Characters` - Implement character name retrieval for a `Star Wars` film using `Promises`

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/'
+
+request(`${API_URL}${process.argv[2]}/`, (error, response, body) => {
+    const characters = JSON.parse(body).characters;
+    const charactersName = characters.map(character_url => new Promise((resolve, reject) => {
+        request(character_url, (error, response, body) => {
+	    resolve(JSON.parse(body).name);
+	});
+    }));
+
+    Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => console.log(allErr));
+});
