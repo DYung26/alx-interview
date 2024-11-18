f441456266bfd644f702c68a02db676790dbc322
DYung26 <theadstore1407@gmail.com>
2024-11-17 19:32:56 -0800

    Add input validation for process arguments in `0-starwars_characters.js`

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,24 @@
 const request = require('request');
 const API_URL = 'https://swapi-api.alx-tools.com/api/films/'
 
-request(`${API_URL}${process.argv[2]}/`, (error, _, body) => {
-  if (error) {
-    console.error(error);
-  }
-  const characters = JSON.parse(body).characters;
-  const charactersName = characters.map(
-    character_url => new Promise((resolve, reject) => {
-      request(character_url, (error, __, body) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(JSON.parse(body).name);
-    });
-  }));
+if (process.argv.length > 2) {
+  request(`${API_URL}${process.argv[2]}/`, (error, _, body) => {
+    if (error) {
+      console.error(error);
+    }
+    const characters = JSON.parse(body).characters;
+    const charactersName = characters.map(
+      character_url => new Promise((resolve, reject) => {
+        request(character_url, (error, __, body) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(JSON.parse(body).name);
+      });
+    }));
 
-  Promise.all(charactersName)
-    .then(names => console.log(names.join('\n')))
-    .catch(allErr => console.log(allErr));
-});
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
