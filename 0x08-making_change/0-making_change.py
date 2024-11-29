a970783b00073834f58bd68f208728e565cd3d08
DYung26 <theadstore1407@gmail.com>
2024-11-29 04:08:35 +0100

    Replace `greedy approach` with `dynamic programming` for optimized `makeChange` function to calculate the minimum number of coins needed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,19 @@
 def makeChange(coins, total):
     if total <= 0:
         return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+'''
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
     coins.sort(reverse=True)
     t_coin = 0
     counter = 0
@@ -13,3 +26,4 @@ def makeChange(coins, total):
         if t_coin == total:
             return counter
     return -1
+'''
